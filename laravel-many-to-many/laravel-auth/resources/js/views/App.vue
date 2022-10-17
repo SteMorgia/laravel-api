9374ee18e2d4f07dd60a6703f687f0ff0eb59958
Stefano Morgia <ste.morgia@gmail.com>
2022-10-17 16:22:16 +0200

    prova push

---
+++ laravel-many-to-many/laravel-auth/resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <MyHeader />
+    </div>
+</template>
+
+<script>
+
+    import MyHeader from '../components/MyHeader.vue';
+    export default {
+        name: 'App',
+        components: {
+            MyHeader,
+        }
+    }
+</script>
